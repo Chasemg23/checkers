dda23b6a154eccf14242e209d15192b7329820c1
Jordan Ward <home@Jordans-MacBook-Pro.local>
2017-11-07 02:25:32 -0500

    Formatting/Style

--- space.h
+++ space.h
@@ -1,26 +1,26 @@
-#include <iostream>
 #ifndef SPACE_H
 #define SPACE_H
 
+#include <iostream>
+
 namespace main_savitch_14 {
 
-class space {
-	public:
-		space()
-			{empty = true; king = false; red = false;}
-		void output() const;
-		space& operator=(const space& s);
-		void setempty(bool b) {empty = b;}
-		void setred(bool b) {red = b;}
-		void setking(bool b) {king = b;}
-		bool isempty() const {return empty;}
-		bool isred() const {return red;}
-		bool isking() const {return king;}
-	private:
-		bool empty;
-		bool red;
-		bool king;
-};
-
+	class space {
+		public:
+            		space() {empty = true; king = false; red = false;}
+            		void output() const;
+            		void operator=(const space& s);
+            		void setempty(bool b) {empty = b;}
+            		void setred(bool b) {red = b;}
+            		void setking(bool b) {king = b;}
+            		bool isempty() const {return empty;}
+            		bool isred() const {return red;}
+            		bool isking() const {return king;}
+        
+        	private:
+            		bool empty;
+           	 	bool red;
+            		bool king;
+    	};
 }
 #endif
