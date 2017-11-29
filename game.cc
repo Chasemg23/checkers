d1dcc354559c2af1e6ad0522ee3c211e93b9ca84
Jordan <jw363912@ohio.edu>
2017-11-28 20:38:07 -0500

    More documentation

--- game.cc
+++ game.cc
@@ -9,17 +9,19 @@ using namespace std;
 namespace main_savitch_14
 {
 
-///@details Clears the screen and then repositions the cursor to the top left of the window
-void game::clear() const
-{
-	std::cout << "\x1B[2J\x1B[H";
-}
+    void game::clear() const{
 
-///@brief Game loop
-///@details Sets the move number to zero, loops through clearing the screen, displaying the board, and receiving input for moves until the game is over
-///@return Human or Computer
-    game::who game::play( )
-    {
+	///@details Clears the screen and then repositions the cursor to the top left of the window
+
+	std::cout << "\x1B[2J\x1B[H";
+    }
+
+    game::who game::play( ){
+
+	///@brief Game loop
+	///@details Sets the move number to zero, loops clearing the screen, displaying the board, and getting input for moves until the game is over
+	///@return Human or Computer
+
 	restart( );
 	while (!is_game_over( ))
 	{
@@ -34,18 +36,21 @@ void game::clear() const
 	return HUMAN;
     }
 
-///@param Constant string message passed by reference
-///@brief Displays the string message to the console
-    void game::display_message(const string& message) const
-    {
+    void game::display_message(const string& message) const{
+
+	///@param Constant string message passed by reference
+	///@brief Displays the string message to the console
+
 	cout << message;
     }
 
-///@brief Gets user's move
-///@details Creates a string to store the input, asks the user to make a move, stores the input, converts all letters to lowercase
-///@return User's move stored in string variable answer
     string game::get_user_move( )
     {
+
+	///@brief Gets user's move
+	///@details Creates a string to store the input, asks the user to make a move, stores the input, converts all letters to lowercase
+	///@return User's move stored in string variable answer
+
 	string answer;
 	
 	display_message("\nMoves are entered this way: a6b5\n");
@@ -59,11 +64,13 @@ void game::clear() const
 	return answer;
     }
 
-///@brief Decides who is currently winning
-///@details Evaluates the board to see which player has more pieces on the board
-///@return Human, Computer, or if the game is currently tied then Neutral
     game::who game::winning( ) const
     {
+
+	///@brief Decides who is currently winning
+	///@details Evaluates the board to see which player has more pieces on the board
+	///@return Human, Computer, or Neutral if the game is currently tied
+
 	int value = evaluate( );
 
 	if (value > 0)
@@ -74,14 +81,16 @@ void game::clear() const
 	    return NEUTRAL;
     }
 
-///@param Integer look_ahead for levels of evaluation
-///@param Integer beat_this to store another move we're considering
-///@brief Looks ahead and decides the best move to make
-///@details look_ahead is how deep the lookahead should go to evaluate the move and beat_this is another move but if the current board position can't beat
-/// this then cut it short
-///@return The best move stored in best_value
     int game::eval_with_lookahead(int look_ahead, int beat_this)
     {
+
+	///@param Integer look_ahead for levels of evaluation
+	///@param Integer beat_this to store another move we're considering
+	///@brief Looks ahead and decides the best move to make
+	///@details look_ahead is how deep the lookahead should go to evaluate the move and beat_this is another move but if the current board position
+	/// can't beat this then cut it short
+	///@return The best move stored in best_value
+
     	queue<string> moves;   ///< All possible opponent moves
 	int value;             ///< Value of a board position after opponent moves
     	int best_value;        ///< Evaluation of best opponent move
@@ -115,10 +124,12 @@ void game::clear() const
     	return -best_value;
     }
 
-///@brief Lets the computer move
-///@details Computes all legal moves, evaluates the moves, decides which is best, and then calls checkers::make_move(move)
     void game::make_computer_move( )
     {
+
+	///@brief Lets the computer move
+	///@details Computes all legal moves, evaluates the move, decides which is best, and then makes the move
+
 	queue<string> moves;
 	int value;
 	int best_value;
@@ -146,11 +157,13 @@ void game::clear() const
 	make_move(best_move);
     }
 
-///@brief Lets the human move
-///@details Creates a string to store the input, calls get_user_move(), if the move is illegal it calls get_user_move() until the input is legal,
-/// and then calls checkers::make_move(move)
     void game::make_human_move( )
     {
+
+	///@brief Lets the human move
+	///@details Creates a string to store the input, gets the user move, if the move is illegal it asks the user for a different move until
+	/// the input is legal and then makes the move
+
         string move;
 	move = get_user_move( );
 	while (!is_legal(move))
