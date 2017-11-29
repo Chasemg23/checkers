036f3a43485d9c0cbdd0ee315ff0637a4e8015f7
Jordan <jw363912@ohio.edu>
2017-11-28 19:35:44 -0500

    Deleting old doxygen generated files

--- html/search/all_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['winning',['winning',['../classmain__savitch__14_1_1checkers.html#aad9a08662e9fdf62752c60997b29370e',1,'main_savitch_14::checkers::winning()'],['../classmain__savitch__14_1_1game.html#a2f0d5338c12bd98d52fe2383ece5c45e',1,'main_savitch_14::game::winning()']]]
-];
--- html/search/functions_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['make_5fmove',['make_move',['../classmain__savitch__14_1_1checkers.html#a8a08c0555d5b8c264472cc94a2f007e3',1,'main_savitch_14::checkers']]]
-];
